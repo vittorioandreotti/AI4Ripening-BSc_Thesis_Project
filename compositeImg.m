42993841e68adebdc571d46ce8ffaafff456f556
Vittorio Andreotti <andreottivittorio@gmail.com>
2020-12-04 11:48:47 +0100

    Creazione (3)

--- compositeImg.m
+++
@@ -1,9 +0,0 @@
-function Iout = compositeImg(I,idxR,idxG,idxB,scale)    
-    d = size(I);
-    Iout = uint8(zeros(d(1),d(2),3));
-    Iout(:,:,1) = normalizeImg(I(:,:,idxR));
-    Iout(:,:,2) = normalizeImg(I(:,:,idxG));
-    Iout(:,:,3) = normalizeImg(I(:,:,idxB));
-    imr = imresize(Iout,scale,'bilinear');
-    %imwrite(imr,'cc.png');
-    %imshow(imr);
--- normalizeImg.m
+++
@@ -1,8 +0,0 @@
-function Ir = normalizeImg(I)
-%m = min(I(:));
-%M = max(I(:));
-I(I<0)=0;
-I(I>=1)=1;
-m = 0;
-M = 1;
-Ir = uint8((I - m) ./ (M - m) *255);
